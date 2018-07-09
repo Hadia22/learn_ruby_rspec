75dee0073814bed451723b173c58b8e79dc304d0
Hadia22 <40692228+Hadia22@users.noreply.github.com>
2018-07-09 23:08:32 +0200

    Update calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,41 @@
 #write your code here
+
+def add(chiffre1, chiffre2)
+	chiffre1 + chiffre2
+end
+
+
+
+def substract(chiffre1, chiffre2)
+	chiffre1 - chiffre2
+end
+
+
+
+def sum( nbre= [])
+nbre.reduce(0,:+)
+end
+
+
+
+def multiply(a,b)
+    a*b
+end
+
+
+
+def power(a,b)
+a**b
+end
+
+
+
+def factorial(n)
+
+if n == 0
+1
+else
+n*factorial(n-1)
+
+end
+end
